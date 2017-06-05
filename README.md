060c69f8015f5466dc776d4518134b80b3c7335d
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-06-05 09:00:07 -0400

    New docs

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # Hyperpaste
 
-[Toothpaste](https://github.com/imcatnoone/toothpaste) for your [Hyper](https://hyper.is).
-Just add `hyperpaste` to your `plugins` in `~/.hyper.js`.
+[Toothpaste](https://github.com/imcatnoone/toothpaste) for your [Hyper](https://hyper.is) (theme).
 
-![Screenshot](https://cloud.githubusercontent.com/assets/5074763/22178079/a2d6fb12-dffb-11e6-9253-f288d4f1af64.png)
+Add `hyperpaste` to your `plugins` in `~/.hyper.js`.
 
-MIT licensed.
+![Screenshot](docs/screenshot.png)
+
+MIT license
 
 ---
-*Another thing by [@lachlanjc](https://twitter.com/lachlanjc).*
+*Another thing by [@lachlanjc](https://lachlanjc.me).*
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-
 const colors = {
-  black       : '#222E33',
-  red         : '#CC6666',
-  green       : '#9DC777',
-  yellow      : '#DBCD7F',
-  blue        : '#769EB3',
-  magenta     : '#C5487A',
-  cyan        : '#73B3C0',
-  white       : '#DAE3E8',
-  lightBlack  : '#465E68',
-  lightRed    : '#E36868',
-  lightGreen  : '#A8FF60',
-  lightBlue   : '#96CBFE',
-  colorCubes  : '#ffffff',
+  black: '#222E33',
+  red: '#CC6666',
+  green: '#9DC777',
+  yellow: '#DBCD7F',
+  blue: '#769EB3',
+  magenta: '#C5487A',
+  cyan: '#73B3C0',
+  white: '#DAE3E8',
+  lightBlack: '#465E68',
+  lightRed: '#E36868',
+  lightGreen: '#A8FF60',
+  lightBlue: '#96CBFE',
+  colorCubes: '#ffffff'
 }
 
 const backgroundColor = colors.black
